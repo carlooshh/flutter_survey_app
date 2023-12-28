faa911e712891b333246c4cf62a76a5a21402f8c
Carlos Reis <carlos.henriquer287@gmail.com>
2023-12-28 11:51:49 -0300

    ensure http adapter calls client post with correct body.

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:faker/faker.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:http/http.dart';
@@ -11,27 +13,39 @@ class HttpAdapter {
 
   HttpAdapter(this.client);
 
-  Future<void> request({required String url, required String method}) async {
-    await client.post(Uri.parse(url), headers: {
-      'content-type': 'application/json',
-      'accept': 'application/json'
-    });
+  Future<void> request(
+      {required String url, required String method, Map? body}) async {
+    final jsonBody = body != null ? jsonEncode(body) : null;
+
+    await client.post(Uri.parse(url),
+        headers: {
+          'content-type': 'application/json',
+          'accept': 'application/json'
+        },
+        body: jsonBody);
   }
 }
 
 @GenerateMocks([Client])
 void main() {
-  test("Shoul call http lib with the correct params", () {
-    // Arrange
-    final client = MockClient();
-    final sut = HttpAdapter(client);
-    final url = faker.internet.httpUrl();
+  late MockClient client;
+  late HttpAdapter sut;
+  late String url;
+  late Map body;
 
+  setUp(() {
+    client = MockClient();
+    sut = HttpAdapter(client);
+    url = faker.internet.httpUrl();
+    body = {'any_key': 'any_value'};
+  });
+
+  test("Shoul call http lib with the correct params", () {
     when(client.post(any, body: anyNamed('body'), headers: anyNamed('headers')))
         .thenAnswer((_) async => Response('', 200));
 
     // Act
-    sut.request(url: url, method: 'post');
+    sut.request(url: url, method: 'post', body: body);
 
     // Assert
     verify(client.post(
@@ -41,6 +55,29 @@ void main() {
         headers: {
           'content-type': 'application/json',
           'accept': 'application/json'
-        }));
+        },
+        body: jsonEncode(body)));
+  });
+
+  test("Shoul call http lib without body param", () {
+    when(client.post(any, body: anyNamed('body'), headers: anyNamed('headers')))
+        .thenAnswer((_) async => Response('', 200));
+
+    // Act
+    sut.request(
+      url: url,
+      method: 'post',
+    );
+
+    // Assert
+    verify(client.post(
+      Uri.parse(
+        url,
+      ),
+      headers: {
+        'content-type': 'application/json',
+        'accept': 'application/json'
+      },
+    ));
   });
 }
